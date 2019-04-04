aa74166619429220c0c64343b5204c21b0fbe32f
tomfriday <tomasz-piatek@go2.pl>
2019-04-04 17:42:33 +0200

    update cw2

--- Kolekcje/src/main/Zakup.java
+++ Kolekcje/src/main/Zakup.java
@@ -1,5 +1,7 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Zakup {
@@ -19,6 +21,8 @@ public class Zakup {
 
     private static String cwiczenie2() {
 
+        // podaj wraz z cena nazwy najtanszego i najdrozszego produktu
+
         BigDecimal lowerPrice = towary.get(0).getCena().min(towary.get(1).getCena());
         BigDecimal higherPrice = towary.get(0).getCena().max(towary.get(1).getCena());
 
@@ -36,20 +40,19 @@ public class Zakup {
             }
             if (towary.get(i).getCena() == lowerPrice) {
                 indexOfLeastExpensiveElement = i;
-
             }
         }
-
         return String.valueOf(towary.get(indexOfMostExpensiveElement)) + String.valueOf(towary.get(indexOfLeastExpensiveElement));
-
-        // podaj wraz z cena nazwy najtanszego i najdrozszego produktu
     }
 
     // TROCHE WALKA BYLA,
-
+    // 1 zapytac sie o returnowanie metody,
+    // 2 zmienne w petli a po za nią
 
 
     private static String cwiczenie3() {
+
+
         // posortuj towary alfabetycznie i wylistuj same nazwy
         return null;
     }
